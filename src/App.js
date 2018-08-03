7c1a8d5b7501d5dddc54f9b32bda33ba639fea26
z3d <925699+z3d@users.noreply.github.com>
2018-08-03 11:01:12 +1000

    Create stateless functional components

--- src/App.js
+++ src/App.js
@@ -8,30 +8,29 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Meat doneness calculator</h1>
-          <MeatChooser/>
-        </header>      
+          <MeatChooser />
+        </header>
       </div>
     );
   }
 }
 
-class DonenessCalculator
-{
-    static calculateTemperature(doneness, meatType){
-      switch (meatType){
-        case "beef":
-        case "lamb":
+class DonenessCalculator {
+  static calculateTemperature(doneness, meatType) {
+    switch (meatType) {
+      case "beef":
+      case "lamb":
         return Data.redMeatRanges[doneness];
 
-        case "chicken":
+      case "chicken":
         return Data.chickenRanges[doneness];
 
-        case "patties":
+      case "patties":
         return Data.pattyRanges[doneness];
-        default:
-          return null;
-      }
+      default:
+        return null;
     }
+  }
 }
 
 class MeatChooser extends Component {
@@ -42,83 +41,68 @@ class MeatChooser extends Component {
     super(props);
     this.changeDoneness = this.changeDoneness.bind(this);
     this.changeMeatType = this.changeMeatType.bind(this);
-    this.state = {targetDonenesses: Data.doneness, meatType: this.initialMeatType, targetDoneness: this.initialDoneness};
+    this.state = { targetDonenesses: Data.doneness, meatType: this.initialMeatType, targetDoneness: this.initialDoneness };
   }
 
-  componentDidMount(){    
+  componentDidMount() {
     let targetTemp = DonenessCalculator.calculateTemperature(this.state.targetDoneness, this.state.meatType)
-    this.setState({targetTemperatureRange: targetTemp});
+    this.setState({ targetTemperatureRange: targetTemp });
   }
 
-  changeDoneness(doneness){
-    this.setState({ targetDoneness: doneness});
+  changeDoneness(doneness) {
+    this.setState({ targetDoneness: doneness });
     let targetTemp = DonenessCalculator.calculateTemperature(doneness, this.state.meatType)
-    this.setState({targetTemperatureRange: targetTemp});
+    this.setState({ targetTemperatureRange: targetTemp });
   }
 
-  filterDonnessesForMeatType(meatType){
-      if (meatType === "chicken" || meatType === "patties"){
-        return Data.doneness.filter(d => d.name === "wellDone")
-      }
-      return Data.doneness;
+  filterDonnessesForMeatType(meatType) {
+    if (meatType === "chicken" || meatType === "patties") {
+      return Data.doneness.filter(d => d.name === "wellDone")
+    }
+    return Data.doneness;
   }
 
-  changeMeatType(meatType){
+  changeMeatType(meatType) {
     let targetDoneness = (meatType === "chicken" || meatType === "patties") ? "wellDone" : this.state.targetDoneness;
-    this.setState({ targetDonenesses: this.filterDonnessesForMeatType(meatType), targetDoneness: targetDoneness});   
+    this.setState({ targetDonenesses: this.filterDonnessesForMeatType(meatType), targetDoneness: targetDoneness });
 
     let targetTemp = DonenessCalculator.calculateTemperature(targetDoneness, meatType);
-    this.setState({targetTemperatureRange: targetTemp, meatType: meatType});
-  } 
-  
-  render(){return (
+    this.setState({ targetTemperatureRange: targetTemp, meatType: meatType });
+  }
+
+  render() {
+    return (
       <div>
-          <MeatTypeChooser onChange={this.changeMeatType}/>   
-          <DonenessChooser onChange={this.changeDoneness} doneesses = {this.state.targetDonenesses}/>       
-          <TemperatureTarget target={this.state.targetTemperatureRange}/>
+        <MeatTypeChooser onChange={this.changeMeatType} />
+        <DonenessChooser onChange={this.changeDoneness} doneesses={this.state.targetDonenesses} />
+        <TemperatureTarget target={this.state.targetTemperatureRange} />
       </div>)
-    }
+  }
 }
 
-class DonenessChooser extends Component {
-    constructor(props) {
-      super(props);
-      this.handleChange = this.handleChange.bind(this);
-    }
+const DonenessChooser = (props) => {
+  const handleChange = e => {
+    props.onChange(e.target.value);
+  }
 
-    handleChange(e) {
-      this.props.onChange(e.target.value);
-    }
-    render(){
-      return (
-        <select onChange={this.handleChange}>{
-           this.props.doneesses.map((doneness) => <option key={doneness.name} value={doneness.name}>{doneness.text}</option>)
-         }</select>
-     );
-    }
-}  
+  return (
+    <select onChange={handleChange}>{
+      props.doneesses.map((doneness) => <option key={doneness.name} value={doneness.name}>{doneness.text}</option>)
+    }</select>)
 
-const TemperatureTarget = (props) =>
-{
+}
+
+const TemperatureTarget = (props) => {
   return <p>Target temperature is: {props.target} &deg;C</p>
 }
 
-class MeatTypeChooser extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
+const MeatTypeChooser = (props) => {
+  const handleChange = (e) => props.onChange(e.target.value);
 
-  handleChange(e) {
-    this.props.onChange(e.target.value);
-  }
-  render(){
-    return (
-      <select onChange={this.handleChange}>{
-        Data.meatTypes.map((meat) => <option key={meat.name} value={meat.name}>{meat.text}</option>)
-      }</select>
-   );
-  }
+  return (<select onChange={handleChange}>{
+    Data.meatTypes.map((meat) => <option key={meat.name} value={meat.name}>{meat.text}</option>)
+  }</select>);
+
 }
 
 export default App;
