741fe0a1eb2d581ca001e1190c48906c1cccf694
z3d <925699+z3d@users.noreply.github.com>
2018-08-06 14:36:46 +1000

    Convert to TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { render } from "react-dom";
 import { Provider } from "react-redux";
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, compose  } from 'redux';
 import store from './store';
-import { changeMeatType, changeDoneness } from './actions/index';
-import { connect } from "react-redux";
 
 ReactDOM.render( <Provider store={store}><App /></Provider>, 
     document.getElementById('root'));
-window.store = store;
-window.changeMeatType = changeMeatType;
-window.changeDoneness = changeDoneness;
 registerServiceWorker();
